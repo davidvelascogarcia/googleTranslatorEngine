4b4bff1aec3ce9927e428934217c4f778090f76a
davidvelascogarcia <davelasc@ing.uc3m.es>
2020-07-26 12:54:16 +0200

    Improve format

--- programs/googleTranslatorEngine.py
+++ programs/googleTranslatorEngine.py
@@ -4,18 +4,15 @@
  *      Type: Python
  *      Author: David Velasco Garcia @davidvelascogarcia
  * ************************************************************
- */
-
-/*
-  *
-  * | INPUT PORT                           | CONTENT                                                 |
-  * |--------------------------------------|---------------------------------------------------------|
-  * | /googleTranslatorEngine/data:i       | Input data text to translate                            |
-  *
-  * | OUTPUT PORT                          | CONTENT                                                 |
-  * |--------------------------------------|---------------------------------------------------------|
-  * | /googleTranslatorEngine/data:o       | Output translated text                                  |
-  *
+ *
+ * | INPUT PORT                           | CONTENT                                                 |
+ * |--------------------------------------|---------------------------------------------------------|
+ * | /googleTranslatorEngine/data:i       | Input data text to translate                            |
+ *
+ * | OUTPUT PORT                          | CONTENT                                                 |
+ * |--------------------------------------|---------------------------------------------------------|
+ * | /googleTranslatorEngine/data:o       | Output translated text                                  |
+ *
 '''
 
 # Libraries
@@ -27,7 +24,6 @@ import platform
 import time
 import yarp
 
-
 print("**************************************************************************")
 print("**************************************************************************")
 print("                  Program: Google Translator Engine                       ")
@@ -38,73 +34,87 @@ print("*************************************************************************
 
 print("")
 print("Starting system ...")
+print("")
 
 print("")
-print("Loading Google Translator engine ...")
-
+print("Loading Google Translator Engine module ...")
 print("")
-print("")
-print("**************************************************************************")
-print("YARP configuration:")
-print("**************************************************************************")
-print("")
-print("Initializing YARP network ...")
 
-# Init YARP Network
-yarp.Network.init()
-
-
-print("")
-print("[INFO] Opening data input port with name /googleTranslatorEngine/data:i ...")
-
-# Open input data port
-googleTranslatorEngine_inputPort = yarp.Port()
-googleTranslatorEngine_inputPortName = '/googleTranslatorEngine/data:i'
-googleTranslatorEngine_inputPort.open(googleTranslatorEngine_inputPortName)
-
-# Create input data bottle
-inputBottle=yarp.Bottle()
-
-print("")
-print("[INFO] Opening data output port with name /googleTranslatorEngine/data:o ...")
-
-# Open output data port
-googleTranslatorEngine_outputPort = yarp.Port()
-googleTranslatorEngine_outputPortName = '/googleTranslatorEngine/data:o'
-googleTranslatorEngine_outputPort.open(googleTranslatorEngine_outputPortName)
-
-# Create output data bottle
-outputBottle=yarp.Bottle()
-
-
-print("")
-print("Initializing googleTranslator engine ...")
-
-# Get system configuration
 print("")
 print("Detecting system and release version ...")
+print("")
 systemPlatform = platform.system()
 systemRelease = platform.release()
 
 print("")
-print("")
 print("**************************************************************************")
 print("Configuration detected:")
 print("**************************************************************************")
+print("")
 print("Platform:")
 print(systemPlatform)
 print("Release:")
 print(systemRelease)
 
 print("")
+print("**************************************************************************")
+print("Initializing googleTranslatorEngine:")
+print("**************************************************************************")
+print("")
+print("[INFO] Initializing googleTranslatorEngine at " + str(datetime.datetime.now()) + " ...")
+print("")
+
+print("")
+print("[INFO] Google Translator Engine initialized correctly.")
+print("")
+
+print("")
+print("**************************************************************************")
+print("YARP configuration:")
+print("**************************************************************************")
+print("")
+print("Initializing YARP network ...")
+print("")
+
+# Init YARP Network
+yarp.Network.init()
+
+print("")
+print("[INFO] Opening data input port with name /googleTranslatorEngine/data:i ...")
+print("")
+
+# Open input googleTranslatorEngine data port
+googleTranslatorEngine_inputPort = yarp.BufferedPortBottle()
+googleTranslatorEngine_inputPortName = '/googleTranslatorEngine/data:i'
+googleTranslatorEngine_inputPort.open(googleTranslatorEngine_inputPortName)
+
+
+print("")
+print("[INFO] Opening data output port with name /googleTranslatorEngine/data:o ...")
+print("")
+
+# Open output googleTranslatorEngine data port
+googleTranslatorEngine_outputPort = yarp.Port()
+googleTranslatorEngine_outputPortName = '/googleTranslatorEngine/data:o'
+googleTranslatorEngine_outputPort.open(googleTranslatorEngine_outputPortName)
+
+# Create googleTranslatorEngine output data bottle
+googleTranslatorEngineOutputBottle = yarp.Bottle()
+
+print("")
+print("[INFO] YARP configured correctly.")
+print("")
+
 print("")
 print("**************************************************************************")
 print("Translator configuration:")
 print("**************************************************************************")
+print("")
 
-loopControlIniExist = 0
+# Control loop file exist
+loopControlFileExist = 0
 
-while int(loopControlIniExist)==0:
+while int(loopControlFileExist) == 0:
 
     try:
         # Get languages data
@@ -114,100 +124,149 @@ while int(loopControlIniExist)==0:
         languagesData.read('../config/languages.ini')
         languagesData.sections()
 
+        # Extract language input
         inputLanguage = languagesData['Languages']['language-input']
-        outputLanguage = languagesData['Languages']['language-output']
-        loopControlIniExist = 1
+
+        # Exit loop
+        loopControlFileExist = 1
+
     except:
         print("")
         print("**************************************************************************")
         print("Error file not founded:")
         print("**************************************************************************")
+        print("")
+
         # File not exist
         print("")
         print("[ERROR] Error, languages.ini not founded, i will check again in 4 seconds ...")
         print("")
         time.sleep(4)
 
+print("")
 print("[INFO] Data obtained correctly.")
 print("")
-print("Input language: "+ str(inputLanguage))
-print("Output language: "+ str(outputLanguage))
 
 print("")
+print("Input language: " + str(inputLanguage))
+print("Output language: Selected by user in real time sending language to translate in message text")
+print("")
+
 print("")
 print("**************************************************************************")
 print("Google Translator client:")
 print("**************************************************************************")
 print("")
-print("Configuring Google Translator client ...")
+print("Configuring Google Translator client at " + str(datetime.datetime.now()) + " ...")
+print("")
 
 googleTranslatorEngineClient = Translator()
 
+print("")
 print("[INFO] Client configuration done.")
+print("")
 
 
-while True:
+# Variable to control loopControlTranslate
+loopControlTranslate = 0
 
-    # Waiting to input data
+print("")
+print("**************************************************************************")
+print("Waiting for text to translate:")
+print("**************************************************************************")
+print("")
+print("[INFO] Waiting for input text to translate at " + str(datetime.datetime.now()) + " ...")
+print("")
+
+while int(loopControlTranslate) == 0:
+
     print("")
-    print("Waiting for input data ...")
+    print("**************************************************************************")
+    print("Processing translate request:")
+    print("**************************************************************************")
+    print("")
+    print("[INFO] Processing translate requests at " + str(datetime.datetime.now()) + " ...")
+    print("")
 
-    googleTranslatorEngine_inputPort.read(inputBottle)
-    dataToTranslate = inputBottle.toString()
+    # Receive text to translate
+    googleTranslatorEngineInputBottle = googleTranslatorEngine_inputPort.read()
+
+    # Extract text to translate
+    dataToTranslate = googleTranslatorEngineInputBottle.get(1).asString()
+
+    # Clear string to remove ""
+    dataToTranslate = str(dataToTranslate)
     dataToTranslate = dataToTranslate.replace('"','')
 
-    print("[RECEIVED] Data received: "+str(dataToTranslate))
+    # Extract language to translate
+    outputLanguage = googleTranslatorEngineInputBottle.get(3).asString()
+
+    # Clear string to remove ""
+    outputLanguage = str(outputLanguage)
+    outputLanguage = outputLanguage.replace('"','')
+
 
     print("")
+    print("[RECEIVED] Data received: " + str(dataToTranslate) + ", translate to " + str(outputLanguage) + " at " + str(datetime.datetime.now()))
     print("")
-    print("**************************************************************************")
-    print("Processing:")
-    print("**************************************************************************")
+
 
     try:
         # Sending request to Google Translator API
         print("")
-        print("Connecting with Google Translator server ...")
+        print("**************************************************************************")
+        print("Connecting with Google Translator server:")
+        print("**************************************************************************")
+        print("")
+        print("[INFO] Connecting with Google Translator server ...")
+        print("")
 
         print("")
-        print("[INFO] Translating from "+str(inputLanguage)+ " to "+ str(outputLanguage)+" ...")
+        print("[INFO] Translating from " + str(inputLanguage) + " to " + str(outputLanguage) + " at " + str(datetime.datetime.now()) + " ...")
+        print("")
 
-        dataTranslated = googleTranslatorEngineClient.translate(str(dataToTranslate),dest=str(outputLanguage), src=str(inputLanguage))
+        # Translating
+        dataTranslated = googleTranslatorEngineClient.translate(str(dataToTranslate), dest=str(outputLanguage), src=str(inputLanguage))
         dataTranslated = dataTranslated.text
-        print("")
-        print("[INFO] Text translated.")
 
         print("")
-        print("Server response done.")
-
+        print("[INFO] Text translated at " + str(datetime.datetime.now()) + ".")
         print("")
+
+        # Show results
         print("")
         print("**************************************************************************")
         print("Results:")
         print("**************************************************************************")
         print("")
-        print("[RESULTS] Input text in "+str(inputLanguage)+" language: "+ str(dataToTranslate))
+        print("[RESULTS] Input text in " + str(inputLanguage) + " language: "+ str(dataToTranslate))
         print("")
-        print("[RESULTS] Output text in "+str(outputLanguage)+" language: "+ str(dataTranslated))
+        print("[RESULTS] Output text in " + str(outputLanguage) +" language: " + str(dataTranslated))
+        print("")
 
     except:
         print("")
         print("[ERROR] Sorry, i could´t resolve your request.")
+        print("")
 
 
     # Send output results
-    outputBottle.clear()
-    outputBottle.addString(str(dataTranslated))
-
-    googleTranslatorEngine_outputPort.write(outputBottle)
+    googleTranslatorEngineOutputBottle.clear()
+    googleTranslatorEngineOutputBottle.addString("TRANSLATION:")
+    googleTranslatorEngineOutputBottle.addString(str(dataTranslated))
+    googleTranslatorEngineOutputBottle.addString("DATE:")
+    googleTranslatorEngineOutputBottle.addString(str(datetime.datetime.now()))
+    googleTranslatorEngine_outputPort.write(googleTranslatorEngineOutputBottle)
 
 # Close YARP ports
-print("Closing YARP ports...")
+print("[INFO] Closing YARP ports ...")
 googleTranslatorEngine_inputPort.close()
 googleTranslatorEngine_outputPort.close()
 
 print("")
-print("")
 print("**************************************************************************")
 print("Program finished")
 print("**************************************************************************")
+print("")
+print("googleTranslatorEngine program finished correctly.")
+print("")
